48c047c96ef94371130d9349b7dba4dc10ee0055
Adam Greenhill <adam.greenhill@bjss.com>
2024-02-28 15:05:30 +0000

    fix restart bug
    fix bug where sharon would attack immediately when starting a new game from the restart button

--- Assets/SharonBehaviour.cs
+++ Assets/SharonBehaviour.cs
@@ -12,7 +12,9 @@ public class SharonBehaviour : MonoBehaviour
 
     private static Random rnd = new Random();
 
-    private float lastAttackTime = 0.0f;
+    private float lastAttackTime;
+       private float birth = 0.0f;
+   private float age = 0.0f;
 
 private enum AttackType {
     Alcohol,
@@ -20,12 +22,16 @@ private enum AttackType {
 
     void Start()
     {
+        Debug.Log("Starting Sharon!");
+        birth = Time.realtimeSinceStartup;
+        lastAttackTime = birth;
         playerTom = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.age=Time.realtimeSinceStartup-birth;
         if (playerTom == null){
             return;
         }
@@ -35,8 +41,8 @@ private enum AttackType {
         //update the position
         transform.position = transform.position + new Vector3(difference.x * movementSpeed * Time.deltaTime, difference.y * movementSpeed * Time.deltaTime, 0);
 
-        if (Time.realtimeSinceStartup - lastAttackTime > 12.0f){
-            lastAttackTime = Time.realtimeSinceStartup;
+        if (age - lastAttackTime > 5.0f){
+            lastAttackTime = age;
             Debug.Log("Sharon is attacking!");
             Array values = AttackType.GetValues(typeof(AttackType));
             AttackType randomAttack = (AttackType)values.GetValue(rnd.Next(values.Length));
